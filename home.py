a4672054ed502425e0f44b28a07c8c626d2b23aa
appnacolleg <zubairkhan4671498@gmail.com>
2024-07-23 07:01:15 +0500

    my home work

--- home.py
+++ home.py
@@ -19,3 +19,101 @@ print("Bismillah")
 print("Bismillah")
 print("Bismillah")
 print("allah tera arab ha arab shukar")
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+def myfunc(n):
+	return lambda a : a*n
+mydouble = myfunc(5)
+print(mydouble(5))
+# bismillah
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
+def my_func(n):
+	return lambda a : a ** n
+mycube = my_func(3)
+print(mycube(5))
+def my_func(n):
+	return lambda a : a * n
+mycube = my_func(3)
+print(mycube(5))
+# bismillah
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[0]
+
+print(x)
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+cars[0] = "Toyota"
+print(cars)
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+
+for x in cars:
+  print(x)
+
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.append("Honda")
+
+print(cars)
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.pop(1)
+
+print(cars)
+# bismillah
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.remove("Volvo")
+
+print(cars)
+
+# bismillah
+fruits = ['apple', 'banana', 'cherry']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+fruits.extend(cars)
+
+print(fruits)
+
+# bismillah
+fruits = ["apple", "banana", "cherry","cherry"]
+
+x = fruits.count("cherry")
+
+print(x)
+
+# bismillah
+points = [1, 4, 2, 9, 7, 8, 9, 3, 1, 9]
+
+x = points.count(9)
+
+print(x)
+
+# bismillah
+
---
+++ while_pr.py
@@ -0,0 +1,796 @@
+# Bismillah alrehman alraheem
+
+# 1. Print numbers from 1 to 5.
+def b():
+    print("Bismillah alrehman alraheem")
+b()
+# Simple countdown from 5 to 1
+# num = 5
+# while num >= 0:
+#     print(num)
+#     num -= 1
+# print("top down")
+
+# # Simple countup from 1 to 5
+# num = 0
+# while num <= 5:
+#     print(num)
+#     num += 1
+# print("bottom up")
+# num = 0
+# while num <= 0:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 1:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 2:
+#     print(num)
+#     num += 1
+# num = 0
+# while num <= 3:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 4:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 5:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 6:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 7:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 8:
+#     print(num)
+#     num += 1
+# b()
+# num = 0
+# while num <= 9:
+#     print(num)
+#     num += 1
+# num = 0
+# while num <= 10:
+#     print(num)
+#     num += 1
+# b()
+# b()
+# b()
+# count = 0
+# while count < 0:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 1:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 2:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 3:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 4:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 6:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 7:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 8:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 9:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 10:
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count < 11:
+#     print(count)
+#     count += 1
+# b()
+# b()
+# b()
+# count = 0
+# while count == 0:
+#     print(count)
+#     count += 1
+# b()
+# count = 1
+# while count == 1:
+#     print(count)
+#     count += 1
+# b()
+# count = 2
+# while count == 2:
+#     print(count)
+#     count += 1
+# b()
+# count = 3
+# while count == 3:
+#     print(count)
+#     count += 1
+# b()
+# count = 4
+# while count == 4:
+#     print(count)
+#     count += 1
+# b()
+# count = 5
+# while count == 5:
+#     print(count)    
+#     count += 1
+# b()
+# count = 0
+# while count == 6:        
+#     print(count)
+#     count += 1          
+# b()
+# count = 0
+# while count == 7:            
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count == 8:                
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count == 9:                    
+#     print(count)
+#     count += 1
+# b()
+# count = 0
+# while count == 10:                        
+#     print(count)
+#     count += 1
+# b()
+# b()
+# b()
+# num = 11
+# while num <= 11:
+#     print(num)
+#     num += 1
+# b()
+# num = 11
+# while num <= 12:
+#     print(num)
+#     num += 1
+# b()
+# num = 11
+# while num <= 13:
+#     print(num)
+#     num += 1
+# b()
+# num = 11
+# while num <= 14:
+#     print(num)
+#     num += 1
+# b()
+# num = 11
+# while num <= 15:
+#     print(num)
+#     num += 1
+# b()
+# num = 15
+# while num <= 15:
+#     print(num)
+#     num += 1
+# b()
+# num = 15
+# while num <= 16:
+#     print(num)
+#     num += 1
+# b()
+# num = 15 
+# while num <= 17:
+#     print(num)
+#     num += 1
+# b()
+# num = 15
+# while num <= 18:
+#     print(num)
+#     num += 1
+# b()
+# num = 15 
+# while num <= 19:
+#     print(num)
+#     num += 1
+# b()
+# num = 15
+# while num <= 20:
+#     print(num)
+#     num += 1
+# b()
+# num = 10
+# while num >= 0:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 1:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10 
+# while num >= 2:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 3:
+#     print(num)      
+#     num -= 1
+# b()
+# num = 10
+# while num >= 4:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 5:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 6:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 7:
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 8:
+#     print(num)
+#     num -= 1            
+# b()
+# num = 10        
+# while num >= 9:            
+#     print(num)
+#     num -= 1
+# b()
+# num = 10        
+# while num >= 10:            
+#     print(num)
+#     num -= 1
+# b()
+# num = 10        
+# while num >= 11:            
+#     print(num)
+#     num -= 1
+# b()
+# num = 10        
+# while num >= 12:            
+#     print(num)
+#     num -= 1
+# b()
+# num = 10        
+# while num >= 13:            
+#     print(num)
+#     num -= 1
+# b()
+# num = 10        
+# while num >= 14:            
+#     print(num)
+#     num -= 1
+# b()
+# num = 10
+# while num >= 0:
+#     print(num)
+#     num -= 1
+# b()
+# num = 0
+# while num <= 10:
+#     print(num)
+#     num += 1
+# b()
+# num = 10
+# while num > 0:
+#     print(num)
+#     num -= 1
+# b()
+# num = 0
+# while num < 10:
+#     print(num)
+#     num += 1
+# b()
+# num = 10
+# while num >= 0:
+#     print(num)
+#     num -= 1
+# b()
+# num = 0
+# while num <= 10:
+#     print(num)
+#     num += 1
+# b()
+# b()
+# x = 11
+# while x <= 20:
+#     print(x)
+#     x += 1
+# b()
+# y = 20
+# while y >= 11:
+#     print(y)
+#     y -= 1
+# b()
+# x = 21
+# while x <= 30:
+#     print(x)
+#     x += 1
+# # b()
+# y = 30 
+# while y >= 21:
+#     print(y)
+#     y -= 1
+# x = 31
+# while x <= 40:
+#     print(x)
+#     x += 1
+# b()
+# y = 40
+# while y >= 31:
+#     print(y)
+#     y -= 1
+# b()
+
+# z = 11
+# while z >= 0:
+#     print(z)
+#     z -= 1
+# b()
+# a = 0
+# while a <= 10:
+#     print(a)
+#     a += 1
+# num = 4 
+# fact =1
+# while num > 1:
+#     fact *= num
+#     num -= 1
+#     print(fact)
+# for i in range(3):
+#     for j in range(3):
+#         print('*', end=' ')
+#     print()
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(f'{i * j:2}', end=' ')
+#     print()
+# i = 1
+# while i <= 3:
+#     j = 1
+#     while j <= 3:
+#         print(i, j)
+#         j += 1
+#     i += 1
+#     print()
+# i = 1
+# while i <= 3:
+#     j = 1
+#     while j <= 3:
+#         print(i, j,end=" ")
+#         j += 1
+#     i += 1
+#     print()
+# # rows = 5
+# # for i in range(1, rows + 1):
+# #     for j in range(rows - i):
+# #         print(' ', end='')
+# #     for k in range(2 * i - 1):
+# #         print('*', end='')
+# #     print()
+for j in range(4):
+    print(".",end="")
+
+print()
+b()
+b()
+b()
+b()
+b()
+b()
+
+# Simple countdown from 5 to 1
+# num = 5
+# while num > 0:
+#     print(num)
+#     num -= 1
+# print("top down")
+# # b()
+# i = 1
+# while i <= 4:
+#     print(i)
+#     i += 1
+# # 2. Print the numbers from 10 to 15.
+# b()
+# num = 10
+# while num <= 15:
+#     print(num)
+#     num += 1
+
+b()
+# Print the first 5 natural numbers.
+
+# num = 1
+# while num <= 5:
+#     print(num)
+#     num += 1
+#  Print numbers from 5 to 1 in reverse order.
+
+# num = 5
+# while num >= 1:
+#     print(num)
+#     num -= 1
+b()
+#  Print all even numbers from 2 to 10.
+
+# num = 2
+# while num <= 10:
+#     print(num)
+#     num += 2
+# b()
+# Print all odd numbers from 1 to 9.
+
+# num = 1
+# while num <= 9:
+#     print(num)
+#     num += 1
+# b()
+# Print numbers from 1 to 3, each on a new line.
+
+# num = 1
+# while num <= 3:
+#     print(num)
+#     num += 1
+b()
+# Print numbers from 1 to 7, but skip the number 4.
+
+# num = 1
+# while num <= 7:
+#     if num != 4:
+#         print(num)
+#     num += 1
+# b()
+# num = 1
+# while num <= 7:
+#     if num != 1:
+#         print(num)
+#     num += 1
+# b()
+# num = 1
+# while num <= 7:
+#     if num == 2:
+#         print(num)
+#     num += 1
+# b()
+# Print numbers from 1 to 4, but only if they are greater than 2.
+
+# num = 1
+# while num <= 4:
+#     if num > 2:
+#         print(num)
+#     num += 1
+# b()
+# Print the first 3 multiples of 3.
+
+# num = 3
+# count = 0
+# while count < 3:
+#     print(num)
+#     num += 3
+#     count += 1
+# b()
+# num = 2
+# count = 0
+# while count < 10:
+#     print(num)
+#     num += 2
+#     count += 1
+# num = 3
+# count = 0
+# while count < 10:
+#     print(num)
+#     num += 3
+#     count += 1
+# b()
+# 11. Print the numbers from 1 to 6 in ascending order.
+
+# num = 1
+# while num <= 6:
+#     print(num)
+#     num += 1
+b()
+# Print the numbers from 7 to 1 in descending order.
+
+# num = 7
+# while num >= 1:
+#     print(num)
+#     num -= 1
+b()
+# Print the first 4 numbers of the Fibonacci sequence.
+
+# a, b = 0, 1
+# count = 0
+# while count < 8:
+#     print(a)
+#     a, b = b, a + b
+#     count += 1
+# Print the first 5 numbers in the sequence where each number is the previous number plus 2.
+
+# num = 1
+# count = 0
+# while count < 5:
+#     print(num)
+#     num += 2
+#     count += 1
+b()
+# Print the squares of numbers from 1 to 3.
+
+# num = 1
+# while num <= 3:
+#     print(num * num)
+#     num += 1
+# num = 3
+# while num <= 6:
+#     print(num ** 2)
+#     num += 1
+# b()
+# Print the first 3 numbers, each multiplied by 2.
+
+# num = 1
+# count = 0
+# while count < 3:
+#     print(num * 2)
+#     num += 1
+#     count += 1
+# b()
+# num = 1
+# # count = 0
+# while num <= 3:
+#     print(num * 2)
+#     num += 1
+#     # count += 1
+b()
+# Print numbers from 1 to 5 with each number squared.
+
+num = 1
+while num <= 5:
+    print(f"{num} square is {num * num}")
+    num += 1
+b()
+# Print numbers from 1 to 4, but only if they are not equal to 2.
+
+# num = 1
+# while num <= 4:
+#     if num != 2:
+#         print(num)
+#     num += 1
+# num = 1
+# while num <= 4:
+#     if num != 1:
+        
+#         print(num)
+        
+#     num += 1
+# Print the first 5 numbers, but only if they are even.
+
+# num = 1
+# count = 0
+# while count < 5:
+#     print(num)
+#     num += 2
+#     count += 1
+# b()
+# num = 3
+# count = 0
+# while count < 5:
+#     print(num)
+#     num += 2
+#     count += 1
+# b()
+# num = 3
+# count = 0
+# while count < 5:
+#     print(num)
+#     num += 3
+#     count += 1
+# b()
+# num = 1
+# count = 1
+# while count <= 5:
+    
+#     print(num*2)
+#     num *= 2
+#     num += 1
+#     count += 1
+# b()
+# num = 1
+# while num <= 5:
+#     print(num * 2)
+#     num += 1
+b()
+#Print numbers from 5 to 9, but skip 7.
+
+# num = 5
+# while num <= 9:
+#     if num != 7:
+#         print(num)
+#     num += 1
+# b()
+# num = 5
+# while num <= 9:
+#     if num == 7:
+#         print(num)
+#     num += 1
+b()
+# # Print numbers from 2 to 12 in steps of 2.
+
+# num = 2
+# while num <= 12:
+#     print(num)
+#     num += 2
+# b()
+# # Print numbers from 2 to 12 in steps of 3.
+
+# num = 3
+# while num <= 12:
+#     print(num)
+#     num += 3
+b()
+# Print numbers from 1 to 4 and print "Done" after the loop.
+
+# num = 1
+# while num <= 4:
+#     print(num)
+#     num += 1
+# print("Done")
+# b()
+# num = 1
+# while num <= 4:
+#     num += 1
+# print("Done")
+b()
+# Print the first 3 numbers of a sequence where each number is 5 less than the previous number.
+
+# num = 10
+# count = 0
+# while count < 3:
+#     print(num)
+#     num -= 5
+#     count += 1
+# b()
+# num = 10
+# while num >= 0:
+#     print(num)
+#     num -= 5
+# b()
+# num = 1
+# count = 0
+# while count < 5:
+#     print(num)
+#     num += 2
+#     count += 1
+# b()
+# num = 1
+# product = 1
+# while num <= 4:
+#     product *= num
+#     num += 1
+# print(product)
+b()
+# num = 1
+# while num <= 4:
+#     if num == 3:
+#         pass# print("skip")
+#     else:
+#         print(num)
+#     num += 1
+# b()
+# num = 5
+# while num <= 9:
+#     if num != 7:
+#         print(num)
+#     else:
+#         print(f"{num} is skip")
+#     num += 1
+    # num = 5
+# while num <= 9:
+#     if num != 7:
+#         print(num)
+#     num += 1
+# b()
+# # num = 5
+# while num <= 9:
+#     if num != 7:
+#         print(num)
+#     num += 1
+# b()
+# num = 1
+# count = 0
+# while count < 5:
+#     print(num)
+#     num += 3
+#     count += 1
+# b()
+# num = 1
+# total = 0
+# count = 1
+# while count <= 5:
+#     total += num
+#     num += 1
+#     count += 1
+# print(total)
+# b()
+# num = 5
+# count = 0
+# while count < 3:
+#     print(num)
+#     num -= 1
+#     count += 1
+num = 1
+total = 0
+while num <= 3:
+    total += num * 2
+    num += 1
+print(total)
+b()
+num = 1
+while num <= 3:
+    if num % 2 == 0:
+        print(num)
+    num += 1
+b()
+num = 1
+while num <= 5:
+    if num not in [3, 4]:
+        print(num)
+    num += 1
